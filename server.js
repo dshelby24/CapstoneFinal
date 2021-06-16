b11ee121a60b0187c5316e216ee18461b50400f3
domynique <dshelbyv@gmail.com>
2021-06-16 16:48:05 -0400

    plzz

--- server.js
+++ server.js
@@ -37,12 +37,11 @@ const contactRouter = require('./routes/route.contact');
  
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build'))
-  })
-}
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static('client/build'));
+          app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      });
 
 app.listen(port, () => {
 console.log(`Server is running on port: ${port}`);
