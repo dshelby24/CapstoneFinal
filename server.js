3f06e1d2a3ff39d38c4c6ce9936c0db9427d201b
domynique <dshelbyv@gmail.com>
2021-06-16 16:40:28 -0400

    plz

--- server.js
+++ server.js
@@ -37,16 +37,12 @@ const contactRouter = require('./routes/route.contact');
  
 
 
-    if (process.env.NODE_ENV === 'production') {
-      // Exprees will serve up production assets
-      app.use(express.static('client/build'));
-    
-      // Express serve up index.html file if it doesn't recognize route
-      const path = require('path');
-      app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-      });
-    }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build'))
+  })
+}
 
 app.listen(port, () => {
 console.log(`Server is running on port: ${port}`);
